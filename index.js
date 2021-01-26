a3e5becbae951f1cc1709f17f593e4597e1b5835
Willem <willem@monsuwe.nl>
2021-01-26 19:18:35 +0100

    Cleanup

--- index.js
+++ index.js
@@ -56,66 +56,6 @@ io.on('connection', function(socket) {
                 socket.emit('pages', pages)
                 socket.emit('page', pages[pageid])
             })
-            socket.on('marker', (marker) => {
-                if (!pages[marker.page]) { return }
-                if (!pages[marker.page].markers[marker.id]) {
-                    pages[marker.page].markers[marker.id] = {
-                        id:     marker.id,
-                        page:   marker.page,
-                        text:   marker.text,
-                        cls:    marker.cls,
-                        player: marker.player
-                    }
-                }
-                pages[marker.page].markers[marker.id].imx = marker.imx
-                pages[marker.page].markers[marker.id].imy = marker.imy
-                io.emit('marker', pages[marker.page].markers[marker.id])
-            })
-            socket.on('area', (area) => {
-                if (!pages[area.page]) { return }
-                if (!pages[area.page].areas[area.id]) {
-                    pages[area.page].areas[area.id] = {
-                        id:     area.id,
-                        page:   area.page,
-                        cls:    area.cls,
-                        color:  area.color,
-                        player: area.player
-                    }
-                }
-                pages[area.page].areas[area.id].imx = area.imx
-                pages[area.page].areas[area.id].imy = area.imy
-                pages[area.page].areas[area.id].imw = area.imw
-                pages[area.page].areas[area.id].imh = area.imh
-                io.emit('area', pages[area.page].areas[area.id])
-            })
-            socket.on('effect', (effect) => {
-                if (!pages[effect.page]) { return }
-                if (!pages[effect.page].effects[effect.id]) {
-                    pages[effect.page].effects[effect.id] = {
-                        id:     effect.id,
-                        page:   effect.page,
-                        color:  effect.color,
-                        player: effect.player
-                    }
-                }
-                pages[effect.page].effects[effect.id].text = effect.text
-                io.emit('effect', pages[effect.page].effects[effect.id])
-            })
-            socket.on('removeeffect', (effect) => {
-                if (!pages[effect.page]) { return }
-                let effecttd = pages[effect.page].effects[effect.id]
-                if (effecttd) {
-                    delete pages[effect.page].effects[effect.id]
-                    io.emit('removeeffect', effecttd)
-                    for (const i in pages[effecttd.page].areas) {
-                        var area = pages[effecttd.page].areas[i]
-                        if (area.color == effecttd.color) {
-                            io.emit('removearea', area)
-                            delete pages[effecttd.page].areas[i]
-                        }
-                    }
-                }
-            })
             socket.on('zoom', (zoom) => {
                 if (!pages[zoom.page]) { return }
                 pages[zoom.page].zoom = zoom
@@ -147,32 +87,71 @@ io.on('connection', function(socket) {
                 socket.disconnect()
                 return
             }
-            socket.on('marker', (marker) => {
-                if (!pages[marker.page]) { return }
-                if (pages[marker.page].markers[marker.id] && pages[marker.page].markers[marker.id].player) {
-                    pages[marker.page].markers[marker.id].imx = marker.imx
-                    pages[marker.page].markers[marker.id].imy = marker.imy
-                    io.emit('marker', pages[marker.page].markers[marker.id])
-                }
-            })
-            socket.on('area', (area) => {
-                if (!pages[area.page]) { return }
-                if (pages[area.page].areas[area.id] && pages[area.page].areas[area.id].player) {
-                    pages[area.page].areas[area.id].imx = area.imx
-                    pages[area.page].areas[area.id].imy = area.imy
-                    io.emit('area', pages[area.page].areas[area.id])
-                }
-            })
-            socket.on('effect', (effect) => {
-                if (!pages[effect.page]) { return }
-                if (pages[effect.page].effects[effect.id] && pages[effect.page].effects[effect.id].player) {
-                    pages[effect.page].effects[effect.id].text = effect.text
-                    io.emit('effect', pages[effect.page].effects[effect.id])
-                }
-            })
             socket.emit('page', pages[found])
-            return
         }
+        socket.on('marker', (marker) => {
+            if (!pages[marker.page]) { return }
+            if (!pages[marker.page].markers[marker.id]) {
+                if (!admin) { return }
+                pages[marker.page].markers[marker.id] = {
+                    id:     marker.id,
+                    page:   marker.page,
+                    text:   marker.text,
+                    cls:    marker.cls,
+                    player: marker.player
+                }
+            }
+            if (!admin && !pages[marker.page].markers[marker.id].player) { return }
+            pages[marker.page].markers[marker.id].imx = marker.imx
+            pages[marker.page].markers[marker.id].imy = marker.imy
+            io.emit('marker', pages[marker.page].markers[marker.id])
+        })
+        socket.on('area', (area) => {
+            if (!pages[area.page]) { return }
+            if (!pages[area.page].areas[area.id]) {
+                pages[area.page].areas[area.id] = {
+                    id:     area.id,
+                    page:   area.page,
+                    cls:    area.cls,
+                    color:  area.color,
+                    player: area.player
+                }
+            }
+            if (!admin && !pages[area.page].areas[area.id].player) { return }
+            pages[area.page].areas[area.id].imx = area.imx
+            pages[area.page].areas[area.id].imy = area.imy
+            pages[area.page].areas[area.id].imw = area.imw
+            pages[area.page].areas[area.id].imh = area.imh
+            io.emit('area', pages[area.page].areas[area.id])
+        })
+        socket.on('effect', (effect) => {
+            if (!pages[effect.page]) { return }
+            if (!pages[effect.page].effects[effect.id]) {
+                pages[effect.page].effects[effect.id] = {
+                    id:     effect.id,
+                    page:   effect.page,
+                    color:  effect.color,
+                    player: effect.player
+                }
+            }
+            pages[effect.page].effects[effect.id].text = effect.text
+            io.emit('effect', pages[effect.page].effects[effect.id])
+        })
+        socket.on('removeeffect', (effect) => {
+            if (!pages[effect.page]) { return }
+            let effecttd = pages[effect.page].effects[effect.id]
+            if (effecttd) {
+                delete pages[effect.page].effects[effect.id]
+                io.emit('removeeffect', effecttd)
+                for (const i in pages[effecttd.page].areas) {
+                    var area = pages[effecttd.page].areas[i]
+                    if (area.color == effecttd.color) {
+                        io.emit('removearea', area)
+                        delete pages[effecttd.page].areas[i]
+                    }
+                }
+            }
+        })
     })
     socket.on('disconnect', () => {
         if (admin) {
